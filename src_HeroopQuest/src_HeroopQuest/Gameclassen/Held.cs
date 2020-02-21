3ef5ba24164ab5cabbd40a3749219996f08cdd25
KerîmSavran <s113175@ap.be>
2020-02-21 11:33:29 +0100

    Heldclas prob toegevoegd

--- src_HeroopQuest/src_HeroopQuest/Gameclassen/Held.cs
+++ src_HeroopQuest/src_HeroopQuest/Gameclassen/Held.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace src_HeroopQuest.Gameclassen
 {
+    enum HeldType { Barbaar, Dwerg, Elf, Tovenaar}
     class Held
     {
+        public string Naam { get; set; }
+        public string Beschrijving { get; private set; }
+        public int MaxIntelligentie { get; private set; }
+        public int MaxLichaam { get; private set; }
+        public int HuidigIntelligentie { get; set; }
+        public int HuidigLichaam { get; set; }
+        public HeldType HeldType { get; private set; }
+        public int AanvalIsDobbelStenen { get; private set; } = 2;
+        public int VerdedigDobbelStenen { get; private set; } = 2;
+        public int LoopDobbelStenen { get; private set; } = 2;
     }
 }
